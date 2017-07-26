4372d7fb287cff071dad8cf71e51dd228b5aab8c
俊 池邊 <shun.ikebe@avantcorp.com>
2017-07-26 11:36:32 +0900

    Entity

--- MyGameCUI/Entity.cs
+++ MyGameCUI/Entity.cs
@@ -23,34 +23,7 @@ namespace MyGameCUI
         public Hand MyHand;
         public Grave MyGrave;
         public BattleField MyBattleField;
-
-
-        /// <summary>
-        /// CardList[n]をAからBに移動させる
-        /// </summary>
-        /// <param name="a">移動元のカードリスト</param>
-        /// <param name="n">移動させるカードの番号</param>
-        /// <param name="b">移動先のカードリスト</param>
-        public void MoveCard(AbstractDeck a,int n,AbstractDeck b)
-        {
-            b.AddCard(a.CardList[0]);
-            a.RemoveCard(0);
-        }
         
-
-        /// <summary>
-        /// 山札から１枚カードを手札に加える
-        /// </summary>
-        public void Draw()
-        {
-            MoveCard(MyDeck,0,MyHand);
-        }
-
-
-
-        
-
-
 
     }
 }
