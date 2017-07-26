f732112ce8f7ea0d009d90a97882142de8f76bec
CHEUK WA CHRISTOPHER LO <cheuk.wa.lo@avantcorp.com>
2017-07-26 12:00:56 +0900

    エフェクト追加

--- MyGameCUI/Effect.cs
+++ MyGameCUI/Effect.cs
@@ -31,9 +31,9 @@ namespace MyGameCUI
     }
 
     /// <summary>
-    /// 一体の体力と攻撃力を上昇させる
+    /// 味方のモンスター一体の体力と攻撃力を上昇させる（！return値未完成）
     /// </summary>
-    class EnchantOneHPStrEffect : Effect
+    class EnchantSingleEffect : Effect
     {
         // フィールド
         private int hpIncre; // 体力上昇値
@@ -41,36 +41,158 @@ namespace MyGameCUI
 
         // コンストラクタ
         /// <summary>
-        /// 一体の体力と攻撃力を上昇させる
+        /// 自分のモンスター一体の体力と攻撃力を上昇させる
         /// </summary>
         /// <param name="hp">体力上昇値</param>
         /// <param name="atk">攻撃力上昇値</param>
-        public EnchantOneHPStrEffect(int hp, int atk)
+        public EnchantSingleEffect(int hp, int atk)
         {
             hpIncre = hp;
             atkIncre = atk;
-            Description += "指定のモンスターを" + hpIncre.ToString("+#;-#;0") + "/" + atkIncre.ToString("+#;-#;0") + "する"; // +/-の符号を強制的に表示させる
+            Description += "召喚時、味方のモンスター一体を" + hpIncre.ToString("+#;-#;0") + "/" + atkIncre.ToString("+#;-#;0") + "する"; // +/-の符号を強制的に表示させる
         }
 
         // メソッド
         /// <summary>
-        /// 味方のカードを指定させ、強化の効果を発揮する
-        /// （！未完成）
+        /// 味方のモンスターを指定させ、強化の効果を発揮する
         /// </summary>
         public override void ApplyEffect()
         {
-            EnchantOneHPAtk();
+            EnchantSingle();
         }
 
         /// <summary>
-        /// 味方のカードを指定させ、強化の効果を発揮する
-        /// （！未完成）
+        /// 味方のモンスターを指定させ、強化の効果を発揮する
         /// </summary>
-        private void EnchantOneHPAtk()
+        private void EnchantSingle()
         {
-            Card targetMonster = TargetAllyMonster(); // TargetAllyMonster()で、プレイヤーやAIに味方のカードを返してもらう
+            Card targetMonster = TargetAllyMonster(); // TargetAllyMonster()で味方のモンスター一体をreturn
             targetMonster.HP += hpIncre;
             targetMonster.Attack += atkIncre;
+        }
+    }
+
+    /// <summary>
+    /// 召喚されたもの以外、自分のモンスター全体の体力と攻撃力を上昇させる（！return値未完成）
+    /// </summary>
+    class EnchantAlliesEffect : Effect
+    {
+        // フィールド
+        private int hpIncre; // 体力上昇値
+        private int atkIncre; // 攻撃力上昇値
+
+        // コンストラクタ
+        /// <summary>
+        /// 召喚されたもの以外、自分のモンスター全体の体力と攻撃力を上昇させる
+        /// </summary>
+        /// <param name="hp">体力上昇値</param>
+        /// <param name="atk">攻撃力上昇値</param>
+        public EnchantAlliesEffect(int hp, int atk)
+        {
+            hpIncre = hp;
+            atkIncre = atk;
+            Description += "召喚時、自分以外の味方のモンスター全てを" + hpIncre.ToString("+#;-#;0") + "/" + atkIncre.ToString("+#;-#;0") + "する"; // +/-の符号を強制的に表示させる
+        }
+
+        // メソッド
+        /// <summary>
+        /// 味方のモンスター全てに強化の効果を発揮する
+        /// </summary>
+        public override void ApplyEffect()
+        {
+            EnchantAllies();
+        }
+
+        /// <summary>
+        /// 味方のモンスター全てに強化の効果を発揮する
+        /// </summary>
+        private void EnchantAllies()
+        {
+            List<Card> targetMonsters = TargetAllAllies(); // TargetAllAllies()で味方のモンスター全てをreturn（List<Card>で）
+            foreach (Card monster in targetMonsters)
+            {
+                monster.HP += hpIncre;
+                monster.Attack += atkIncre;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 相手のモンスター一体にダメージを与える（！return値未完成）
+    /// </summary>
+    class DamageSingleEffect : Effect
+    {
+        // フィールド
+        private int damage; // ダメージ
+
+        // コンストラクタ
+        /// <summary>
+        /// 相手のモンスター一体にダメージを与える
+        /// </summary>
+        /// <param name="dam">ダメージ</param>
+        public DamageSingleEffect(int dam)
+        {
+            damage = dam;
+            Description += "召喚時、相手のモンスター一体に" + damage.ToString() + "ダメージ"; // ToStringを一致させるべき？
+        }
+
+        // メソッド
+        /// <summary>
+        /// 相手のモンスターを指定させ、ダメージを与える
+        /// </summary>
+        public override void ApplyEffect()
+        {
+            DamageSingle();
+        }
+
+        /// <summary>
+        /// 相手のモンスターを指定させ、ダメージを与える
+        /// </summary>
+        private void DamageSingle()
+        {
+            Card targetEnemy = TargetEnemyMonster(); // TargetEnemyMonster()で相手のモンスター一体をreturn
+            targetEnemy.HP -= damage;
+        }
+    }
+
+    /// <summary>
+    /// 相手のモンスター全てにダメージを与える（！return値未完成）
+    /// </summary>
+    class DamageEnemiesEffect : Effect
+    {
+        // フィールド
+        private int damage; // ダメージ
+
+        // コンストラクタ
+        /// <summary>
+        /// 相手のモンスター全てにダメージを与える
+        /// </summary>
+        /// <param name="dam">ダメージ</param>
+        public DamageEnemiesEffect(int dam)
+        {
+            damage = dam;
+            Description += "召喚時、相手のモンスター全てに" + damage.ToString() + "ダメージ"; // ToStringを一致させるべき？
+        }
+
+        // メソッド
+        /// <summary>
+        /// 相手のモンスターを指定させ、ダメージを与える
+        /// </summary>
+        public override void ApplyEffect()
+        {
+            DamageEnemies();
+        }
+
+        /// <summary>
+        /// 相手のモンスターを指定させ、ダメージを与える
+        /// </summary>
+        private void DamageEnemies()
+        {
+            List<Card> targetEnemies = TargetAllEnemies(); // TargetAllEnemies()で相手のモンスター全てをreturn（List<Card>で）
+            foreach (Card enemy in targetEnemies)
+            {
+                enemy.HP -= damage;
+            }
         }
     }
 }
