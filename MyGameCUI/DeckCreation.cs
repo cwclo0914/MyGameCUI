b0efa49db2e1f4bda29b9706edffdd3c944b51ef
CHEUK WA CHRISTOPHER LO <cheuk.wa.lo@avantcorp.com>
2017-07-26 18:27:01 +0900

    DeckCreation

--- MyGameCUI/DeckCreation.cs
+++ MyGameCUI/DeckCreation.cs
@@ -9,7 +9,7 @@ namespace MyGameCUI
     class DeckCreation
     {
         // フィールド
-        private List<Card> deck = new List<Card>();
+
 
         // メソッド
         /// <summary>
@@ -17,10 +17,9 @@ namespace MyGameCUI
         /// （最終的にはデータベースから引っ張ってきたい）
         /// </summary>
         /// <returns>デッキ"List<Card>"を返す</returns>
-        public List<Card> Create(Entity owner)
+        public void Create(Entity owner)
         {
-            deck.Add(new Monster("ゴブリン", 1, Settings.Aggro, 1, 1, new EnchantSingleEffect(owner, 2, 1)));
-            return deck;
+            owner.MyDeck.AddCard(new Monster("ゴブリン", 1, Settings.Aggro, 1, 1, new EnchantSingleEffect(owner, 2, 1)));
         }
 
     }
